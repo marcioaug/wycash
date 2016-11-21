22bc94fd69099825ac3fae41d07b50f43e420966
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2016-11-21 16:50:52 -0300

    Eliminando o valor duplicado do amount inicial :confounded:

--- src/main/java/net/marcioguimaraes/wycash/model/Dollar.java
+++ src/main/java/net/marcioguimaraes/wycash/model/Dollar.java
@@ -5,11 +5,11 @@ public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
-        this.amount = 5 * 2;
+        this.amount = this.amount * 2;
     }
 
 }
