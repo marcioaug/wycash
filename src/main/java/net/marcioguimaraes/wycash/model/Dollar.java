ac6cb5c9c7ffc1f8878d525be95f5af0cc9dacfa
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2016-11-21 16:26:41 -0300

    teste para o método times da classe Dollar.

---
+++ src/main/java/net/marcioguimaraes/wycash/model/Dollar.java
@@ -0,0 +1,15 @@
+package net.marcioguimaraes.wycash.model;
+
+public class Dollar {
+
+    public int amount;
+
+    public Dollar(int amount) {
+
+    }
+
+    public void times(int multiplier) {
+
+    }
+
+}
---
+++ src/test/java/net/marcioguimaraes/DollarTest.java
@@ -0,0 +1,15 @@
+package net.marcioguimaraes;
+
+import net.marcioguimaraes.wycash.model.Dollar;
+import org.junit.*;
+
+public class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        Assert.assertEquals(10, five.amount);
+    }
+
+}
