926a2b45cc23b85fecc83240f69bfb886e2feab1
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2016-11-21 16:47:30 -0300

    Quando times for chamado no teste é para amount ficar igual o resultado de 5 * 2.

--- src/main/java/net/marcioguimaraes/wycash/model/Dollar.java
+++ src/main/java/net/marcioguimaraes/wycash/model/Dollar.java
@@ -2,14 +2,14 @@ package net.marcioguimaraes.wycash.model;
 
 public class Dollar {
 
-    public int amount = 10;
+    public int amount;
 
     public Dollar(int amount) {
 
     }
 
     public void times(int multiplier) {
-
+        this.amount = 5 * 2;
     }
 
 }
