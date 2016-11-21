a4ae078a2d4ad7b2a30a79b2e09ee4b0d98fcc13
Marcio Augusto Guimarães <marcioaugustosg@gmail.com>
2016-11-21 17:05:44 -0300

    Resolvendo o efeito colateral.

--- src/test/java/net/marcioguimaraes/DollarTest.java
+++ src/test/java/net/marcioguimaraes/DollarTest.java
@@ -8,10 +8,10 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assert.assertEquals(10, five.amount);
-        five.times(3);
-        Assert.assertEquals(15, five.amount);
+        Dollar product = five.times(2);
+        Assert.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assert.assertEquals(15, product.amount);
     }
 
 }
